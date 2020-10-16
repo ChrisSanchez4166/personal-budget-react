16238ec9795e2ecd58628eb7660d5e03dad4ab95
LAPTOP-CNKFUT31\sanch <sanchezchris>
2020-10-15 20:52:25 -0400

    Brought personal Budget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="bottom">
+        <footer className="center">
+            All rights reserved &copy; Fabio Nolasco
+        </footer>
+    </div>
+  );
+}
+
+export default Footer;
